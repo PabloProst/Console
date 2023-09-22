1b2982e5573dcb6c3ac51949b7fb7c634f422430
PabloProst <pabloprost16@gmail.com>
2023-09-22 20:42:20 +0200

    Se añadió la funcion de encendido con sonido

---
+++ js/main..js
@@ -0,0 +1,36 @@
+/*ENCENDIDA DE PANTALLA*/
+
+let pantalla = document.getElementById("nintendoscreen");
+
+
+document.addEventListener("click", (e)=>{
+
+
+    if(e.target.id === 'powerboton'){
+
+
+        pantalla.style.backgroundImage = "url('img/intro.gif')";
+    
+
+    } 
+    
+
+});
+
+/* SONIDO DE ENCENDIDO */
+
+let sound = new Audio('../sounds/on.mp3');
+
+powerboton.addEventListener('click', ()=>{
+
+    sound.play();
+});
+
+
+let encendido = document.getElementById("ledoff");
+
+powerboton.addEventListener('click', ()=>{
+
+encendido.style.backgroundColor = ('#FF0000');
+
+});
